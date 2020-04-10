92f3e7c25ea14d18af8e00fc8a3d6968bb9e381e
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-10 16:26:07 +0300

    Tabbed Content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
 
 // Compose a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -11,3 +12,7 @@ dropdowns.forEach((dropdown) => {
 	const instance = new Dropdown(dropdown);
 	instance.init();
 });
+
+// Compose tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
