164fd7aaf5ab37a6ed7c5351a3e93d2f44cf6140
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-10 16:55:57 +0300

    Snackbars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
 import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
 // Compose a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -16,3 +17,12 @@ dropdowns.forEach((dropdown) => {
 // Compose tabs
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+// Compose snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+	snackbar.show('You make it :)');
+});
