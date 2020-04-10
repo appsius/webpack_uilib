6ba7b2a57ff59a5265da04320b50ab98d85f1d9d
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-10 12:08:21 +0300

    Dropdowns

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
 
-// Create a tooltip
+// Compose a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
 tooltip.init();
+
+// Compose dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach((dropdown) => {
+	const instance = new Dropdown(dropdown);
+	instance.init();
+});
